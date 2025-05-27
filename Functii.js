e1b343a9e6c4b06d90600fb5b0d78aed4e1a7fc4
SterpIoan <nicasterp1@gmail.com>
2025-05-27 09:26:43 +0300

    Update Functii.js

--- Functii.js
+++ Functii.js
@@ -7,12 +7,7 @@ function pariaza(elev) {
         rezultat.textContent = "Completează corect nota și miza!";
         return;
     }
-    const cote = {
-        rares: [40,25,12,6,3,4.5,7,15,50,60],
-        sim:   [60,40,20,15,12,8,3.5,2.5,5.5,6.5],
-        anonim:[60,40,25,15,8,4.5,2.5,3,5.5,6],
-        mihai: [30, 20, 10, 7, 5, 4, 3, 2, 1.5, 1]
-    };
+
 
     const numeElevi = {
         rares: "Rares",
